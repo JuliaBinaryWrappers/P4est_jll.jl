e4cdfe876c8f1177cfd6cba9eb2038ec0d4adbb1
jlbuild <juliabuildbot@gmail.com>
2022-07-25 18:51:10 +0000

    P4est_jll build 2.8.1+0

--- src/P4est_jll.jl
+++ src/P4est_jll.jl
@@ -2,6 +2,8 @@
 baremodule P4est_jll
 using Base
 using Base: UUID
+using LazyArtifacts
+Base.include(@__MODULE__, joinpath("..", ".pkg", "platform_augmentation.jl"))
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("P4est")
